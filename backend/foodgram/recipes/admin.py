b572cc05384958c5fe19f3513f11c41426f1188b
Евгений Аркатов <thejekaarkatov@yandex.ru>
2024-09-01 16:23:58 +0300

    add admins

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit')
+    list_filter = ('name', )
+    search_fields = ('name', )
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    list_editable = ('name', 'slug')
+    empty_value_display = 'пусто'
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author', 'in_favorites')
+    list_editable = ('name',)
+    readonly_fields = ('in_favorites',)
+    list_filter = ('name', 'author', 'tags')
+    empty_value_display = 'пусто'
+
+    @admin.display(description='В избранном')
+    def in_favorites(self, obj):
+        return obj.favorited_by.count()
+
+
+@admin.register(models.RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'ingredient', 'amount')
+    list_editable = ('recipe', 'ingredient', 'amount')
+
+
+@admin.register(models.Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    list_editable = ('user', 'recipe')
+
+
+@admin.register(models.ShoppingList)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    list_editable = ('user', 'recipe')
--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'id',
+        'email',
+        'password',
+        'first_name',
+        'last_name',
+    )
+    list_editable = ('password', )
+    list_filter = ('username', 'email')
+    search_fields = ('username', 'email')
+    empty_value_display = 'пусто'
+
+
+@admin.register(models.Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'user',
+                    'author'
+    )
+    search_fields = ('user',)
+    empty_value_display = 'пусто'
+
