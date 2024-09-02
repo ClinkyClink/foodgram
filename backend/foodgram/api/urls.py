25197cdd51520ce4bb10dc3c0226e2ee70ada73b
Евгений Аркатов <thejekaarkatov@yandex.ru>
2024-09-02 16:39:32 +0300

    Починил статику

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -2,15 +2,19 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import IngredientViewSet, RecipeViewSet, TagViewSet
+from users.views import CustomUserViewSet
 
 app_name = 'api'
 
-router = DefaultRouter()
+v1_router = DefaultRouter()
 
-router.register('ingredients', IngredientViewSet)
-router.register('tags', TagViewSet)
-router.register('recipes', RecipeViewSet)
+v1_router.register(r'ingredients', IngredientViewSet)
+v1_router.register(r'tags', TagViewSet)
+v1_router.register(r'recipes', RecipeViewSet)
+v1_router.register(r'users', CustomUserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/', include(v1_router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.authtoken')),
 ]
