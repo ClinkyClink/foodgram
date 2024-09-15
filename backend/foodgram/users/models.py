201fa0fe180bb5b520625dc72141c02b56ccbeae
Евгений Аркатов <thejekaarkatov@yandex.ru>
2024-09-15 15:23:03 +0300

    isort

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.exceptions import ValidationError
 from django.db import models
-
 from recipes.constants import (MAX_LENGTH_EMAIL, MAX_LENGTH_ROLE,
                                MAX_LENGTH_USER)
 
