4e9f3ef4e90b5027c97cd90e314ff6fa2062255a
Евгений Аркатов <thejekaarkatov@yandex.ru>
2024-09-16 20:59:37 +0300

    adf

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -65,7 +65,7 @@ class RecipeAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
-        formset = RecipeIngredientInline.get_formset(request, obj, form=form)
+        formset = self.get_formset(request, obj, form=form)
         formset.save()
         if obj.recipeingredients.count() == 0:
             obj.recipeingredients.add(formset.instance)
